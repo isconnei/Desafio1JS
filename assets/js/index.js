9f02fbfa9e9783427999b727f5ea9fc1eee25900
isconnei <icontrerasn@pm.me>
2024-08-21 23:24:28 -0400

    se crea función para calcular el total

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,19 @@
 precio = 400000;
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio;
+
+/****************************/
+/*** Inicio función total ***/
+/****************************/
+
+function total() {
+  //obtengo elementos necesarios del DOM
+  cantidadSpan = document.querySelector(".cantidad").innerHTML;
+  valorTotalSpan = document.querySelector(".valor-total");
+
+  //se despliega en pantalla calculo del total
+  valorTotalSpan.innerHTML = Number(cantidadSpan) * Number(precio);
+}
+/***************************/
+/**** Fin función total ****/
+/***************************/
