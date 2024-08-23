effffb20ea367caacb5f392f74ebdfebfb1f2fa0
luis <l.a.espinozaolivares@gmail.com>
2024-08-23 01:39:14 -0400

    funcion +

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,22 @@
+
+
+
 precio = 400000;
+let suma = 0;
+
+
 precioSpan = document.querySelector(".precio-inicial");
+quantitySpan = document.querySelector(".cantidad");
+const botonMas = document.querySelector(".botonMas");
+
 precioSpan.innerHTML = precio;
+
+quantitySpan.innerHTML = suma;
+
+
+botonMas.addEventListener("click", () => {
+suma++;
+quantitySpan.innerHTML = suma; 
+});
+
+
